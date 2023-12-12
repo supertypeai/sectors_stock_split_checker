fcd70fc30ec2f452f0a39caa7e09a67f27d303ef
BrianLoe <corneliusbrian.loe@students.mq.edu.au>
2023-12-12 21:21:17 +1100

    first commit

---
+++ stock_split_checker.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from supabase import create_client
+from dotenv import load_dotenv
+load_dotenv()
+
+class StockSplitChecker:
+    def __init__(self, supabase_client) :
+        self.url = "https://sahamidx.com/?view=Stock.Split&path=Stock"
+        self.supabase_client = supabase_client
+        self.records = []   
+        self.current_date = datetime.today().strftime('%Y-%m-%d')
+    
+    def get_stock_split_records(self) :
+        response = requests.get(self.url)
+        if response.status_code != 200 :
+            raise Exception("Error retrieving data from SahamIDX")
+        
+        soup = BeautifulSoup(response.text, "lxml")
+        table = soup.find('table', {'class':'tbl_border_gray'})
+        rows = table.find_all('tr', recursive=False)[1:]
+        for row in rows :
+            if len(row.find_all('td'))>2 :
+                values = row.find_all('td')
+                date = datetime.strptime(values[-2].text.strip(), '%d-%b-%Y').strftime('%Y-%m-%d')
+                if date<=self.current_date :
+                    break
+                data_dict = {
+                    'symbol':values[1].find('a').text.strip()+'.JK',
+                    'date':date,
+                    'split_ratio':int(values[4].text.strip())
+                }
+                self.records.append(data_dict)
+
+    def upsert_to_db(self):
+        if not self.records:
+            raise Exception("No records to upsert to database")
+        
+        self.supabase_client.table('idx_future_stock_split').upsert(self.records).execute()
+        
+if __name__ == "__main__":
+    url, key = os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY')
+    supabase_client = create_client(url, key)
+    
+    stock_split_checker = StockSplitChecker(supabase_client)
+    stock_split_checker.get_stock_split_records()
+    stock_split_checker.upsert_to_db()
